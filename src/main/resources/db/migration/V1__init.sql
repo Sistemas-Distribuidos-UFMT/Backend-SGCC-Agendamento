7e3e8342838299a973b13cc5e7ebcaa0155b5be5
Erick Rodrigues da Costa <127148221+erickrodricosta@users.noreply.github.com>
2025-08-28 15:59:27 -0400

    feture: adcionado codigo sql

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,62 @@
+DO $$ BEGIN
+    PERFORM 'public.tipo_pessoa'::regtype;
+EXCEPTION
+    WHEN undefined_object THEN
+create type tipo_pessoa as enum ('CLIENTE', 'MEDICO', 'ATENDENTE', 'GESTOR');
+END $$;
+
+DO $$ BEGIN
+    PERFORM 'public.situacao'::regtype;
+EXCEPTION
+    WHEN undefined_object THEN
+create type situacao as enum ('AGENDADA', 'CANCELADA', 'REALIZADA');
+END $$;
+
+create table pessoa(
+    codigo_pessoa uuid primary key,
+    nome varchar(80) not null,
+    email varchar(80) unique not null,
+    telefone varchar(15),
+    senha_criptografada varchar(256) not null,
+    tipo_pessoa tipo_pessoa not null,
+    data_atualizacao timestamp,
+    data_criacao timestamp default current_timestamp
+);
+
+create table especialidade(
+    codigo_especialidade uuid primary key,
+    nome varchar(80) unique not null,
+    descricao text,
+    data_atualizacao timestamp,
+    data_criacao timestamp default current_timestamp
+);
+
+create table medico (
+    codigo_medico uuid primary key,
+    codigo_pessoa uuid references pessoa(codigo_pessoa) unique not null,
+    codigo_especialidade uuid references especialidade(codigo_especialidade) not null,
+    crm varchar(50) unique not null,
+    data_atualizacao timestamp,
+    data_criacao timestamp default current_timestamp
+);
+
+create table expediente (
+    codigo_expediente uuid primary key default gen_random_uuid(),
+    codigo_medico uuid references medico(codigo_medico) not null,
+    dia_da_semana smallint not null,
+    hora_inicio time not null,
+    hora_fim time not null,
+    data_atualizacao timestamp,
+    data_criacao timestamp default current_timestamp
+);
+
+create table consulta (
+    codigo_consulta uuid primary key default gen_random_uuid(),
+    codigo_pessoa uuid references pessoa(codigo_pessoa) not null,
+    codigo_medico uuid references medico(codigo_medico) not null,
+    data timestamp not null,
+    situacao situacao,
+    data_atualizacao timestamp,
+    data_criacao timestamp default current_timestamp,
+    CONSTRAINT medico_horario_unico UNIQUE (codigo_medico, data)
+);
