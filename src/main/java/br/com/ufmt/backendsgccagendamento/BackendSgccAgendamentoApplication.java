57b057398aa8fea587ffb451d2569612adbb61c6
Erick Rodrigues da Costa <127148221+erickrodricosta@users.noreply.github.com>
2025-08-28 14:26:56 -0400

    first commit

---
+++ src/main/java/br/com/ufmt/backendsgccagendamento/BackendSgccAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufmt.backendsgccagendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSgccAgendamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSgccAgendamentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ufmt/backendsgccagendamento/BackendSgccAgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufmt.backendsgccagendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSgccAgendamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
