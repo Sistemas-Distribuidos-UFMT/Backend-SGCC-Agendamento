d26cf4b526237ec862bb554f0a0d0ae30f1da1c0
Erick Rodrigues da Costa <127148221+erickrodricosta@users.noreply.github.com>
2025-09-02 23:48:50 -0400

    fix: corrigido o logout

--- src/main/java/br/com/ufmt/backendsgccagendamento/security/components/CustomLogoutSuccessHandler.java
+++ src/main/java/br/com/ufmt/backendsgccagendamento/security/components/CustomLogoutSuccessHandler.java
@@ -2,6 +2,8 @@ package br.com.ufmt.backendsgccagendamento.security.components;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -11,8 +13,20 @@ import java.io.IOException;
 @Component
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
+    public static final String AUTH_TOKEN = "auth_token";
+
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+
+        ResponseCookie cookie = ResponseCookie.from(AUTH_TOKEN, "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().flush();
     }
