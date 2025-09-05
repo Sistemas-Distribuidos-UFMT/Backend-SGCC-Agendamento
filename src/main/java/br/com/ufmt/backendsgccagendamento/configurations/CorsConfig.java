4a70610bdb3ddcc285bfc71b089d34723cb34cec
Erick Rodrigues da Costa <127148221+erickrodricosta@users.noreply.github.com>
2025-09-04 21:17:05 -0400

    feat: adicionado CorsConfig

--- src/main/java/br/com/ufmt/backendsgccagendamento/configurations/CorsConfig.java
+++ src/main/java/br/com/ufmt/backendsgccagendamento/configurations/CorsConfig.java
@@ -13,6 +13,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
